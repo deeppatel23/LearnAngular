cbd3fe3104364b913ddcadf9862b5e4497a0dd1c
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-02-11 13:53:21 +0530

    custom validation is forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,13 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
         <label>Email:</label>
         <input type="email" formControlName="email">
       </div>
+      <div>
+        <label>Age:</label>
+        <input type="number" formControlName="age">
+        <div *ngIf="form.get('age').errors?.invalidAge">
+          Age must be between 18 and 100.
+    </div>
+  </div>
       <button [disabled]="form.invalid">Submit</button>
     </form>
   `
@@ -25,7 +32,16 @@ export class AppComponent {
   ngOnInit() {
     this.form = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
+      age:  [Validators.required, this.validateAge.bind(this, 18, 100)]
     });
+  }
+
+  validateAge(min: number, max: number, control: FormControl) {
+    const age = control.value;
+    if (age < min || age > max) {
+      return { invalidAge: true };
+    }
+    return null;
   }
 }
